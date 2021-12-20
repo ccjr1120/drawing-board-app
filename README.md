bf1c82674ec69dd8ed37c22d126085c8cb4f443f
ccjr <qianaiwuyu@gmail.com>
2021-12-20 15:26:05 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# sketch-app
+
+## introduce
+
+- use ts.
+- Tools: Pencil, Eraser, Shape Draw,Select, Drag Canvas
---
+++ config/webpack.common.js
@@ -0,0 +1,36 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
+const srcDir = path.join(__dirname, '../src');
+
+module.exports = {
+  entry: {
+    main: path.join(srcDir, '/index.tsx'),
+  },
+  output: {
+    path: path.join(__dirname, '../dist'),
+    filename: '[name].[chunkhash:8].js',
+    chunkFilename: 'chunk/[name].[chunkhash:8].js',
+  },
+  resolve: {
+    alias: { '~': srcDir },
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [
+    new webpack.DefinePlugin({ FONT_BASE: 16 }),
+    new HtmlWebpackPlugin({
+      title: 'template',
+      template: `${srcDir}/index.html`,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)?$/,
+        use: ['babel-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
